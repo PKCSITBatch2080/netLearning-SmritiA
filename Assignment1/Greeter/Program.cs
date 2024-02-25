148dc4fb3ff84e34a9461188952642a880aba78c
asmriti <smritiaryal331@gmail.com>
2024-02-25 17:27:59 +0545

    assignment question 3

--- Assignment1/Greeter/Program.cs
+++ Assignment1/Greeter/Program.cs
@@ -1,7 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 
- string fullName = "Smriti Aryal";
+class Program 
+{
+	static void Main(string[] args)
+	{
+		Console.Write("Enter your full name: ");
+		string fullName = Console.ReadLine();
+		string cFullName = fullName.ToUpper();
 
- string cFullName = fullName.ToUpper();
-Console.WriteLine(cFullName);
+
+		Console.Write("Enter your DOB (yyyy/MM/DD): ");
+		string dobString = Console.ReadLine();
+		DateTime dob = DateTime.Parse(dobString);
+
+		Console.WriteLine($"Hello {fullName}!");
+		Console.WriteLine($"Your DOB is :{dob.ToString("dddd, dd MMMM yyyy")}");
+	}
+}
+
+
+
